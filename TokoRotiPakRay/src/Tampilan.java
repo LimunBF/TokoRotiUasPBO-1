3475ef6d741eddaaf1ac9d4df5e5c2d93e35b3b1
defri105 <defriansyah105@student.uns.ac.id>
2023-06-26 18:27:47 +0700

    Tampilan

---
+++ TokoRotiPakRay/src/Tampilan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+Scanner input= new Scanner
+
+public static void main(String[] args)
+
+System.out.println("-------------------------")
+System.out.println("Toko Roti PakRay")
+System.out.println("-------------------------")
+System.out.println("Pilihan Roti:")
+System.out.println("1. Roti Manis")
+System.out.println("2. Roti Tawar")
+System.out.println("3. Pizza")
+System.out.println("Pilihan Roti:")
+System.out.println(" ")
