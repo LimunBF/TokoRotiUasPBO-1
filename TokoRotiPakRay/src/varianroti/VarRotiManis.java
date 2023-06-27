94d737c061d3ac9a40e734182a2e356dfed3e0ef
oktaviay <oktaviayunita@student.uns.ac.id>
2023-06-27 08:13:28 +0700

    Update VarRotiManis.java

--- TokoRotiPakRay/src/varianroti/VarRotiManis.java
+++ TokoRotiPakRay/src/varianroti/VarRotiManis.java
@@ -25,6 +25,17 @@ public class VarRotiManis extends RotiManis implements ToppingFilling {
 	double HargaFinalVar2Roman = (Math.ceil((HargaVar2RotiManis * 1.4) /100) *100);
 
 	return HargaFinalVar2Roman;
+
+	public double RotiManisvar3() {
+	
+	double HargaTopFillKejuVar3 = (10 / BeratKeju * HargaKeju);
+	double HargaTopFillSosisVar3 = (10 / BeratSosis * HargaSosis);
+
+	double HargaVar3RotiManis = HargaRotiManisPCS() + (HargaTopFillKejuVar3 + HargaTopFillSosisVar3);
+	double HargaFinalVar3Roman = (Math.ceil((HargaVar3RotiManis * 1.4) / 100) *100);
+
+	return HargaFinalVar3Roman;
+	}
 }
 	
 }
